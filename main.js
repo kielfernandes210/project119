8161e55a316f434712beca1e17d5813e72430f8d
kielfernandes210 <87280797+kielfernandes210@users.noreply.github.com>
2021-07-17 14:15:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+function setup()
+
+
+{
+canvas=createCanvas(300,300);
+canvas.position(300, 300);
+video=createCapture(VIDEO);
+video.hide();
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fEtHzVCRu/model.json', modelLoaded);
+}
+
+function modelLoaded(){
+    console.log('Model Loaded');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
+
+
+
+
+
+
+
